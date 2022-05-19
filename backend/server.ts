60401f991d97c291ed2b99f08094fea770933648
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-05-19 11:38:22 +0200

    allow all cors origins

--- backend/server.ts
+++ backend/server.ts
@@ -10,9 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(json());
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 
 app.use(departmentRouter);
 app.use(employeeRouter);
